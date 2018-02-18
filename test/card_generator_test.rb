0f79179637ed7f00d778e70ef457e06a5d590afb
Sabrina Robinson <sabrinarob@gmail.com>
2018-02-18 11:57:47 -0700

    Updating the testing for the card generator to test it can read each card.

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -10,11 +10,17 @@ class CardGeneratorTest < Minitest::Test
   def setup
     filename = File.dirname(__FILE__) + '/../cards.txt'
     @cards = CardGenerator.new(filename).cards
-    binding.pry
   end
 
   def test_it_exists
-    assert_instance_of CardGenerator, @cards
+    assert_equal @cards[0].question, "What is 5 + 5?"
+    assert_equal @cards[0].answer, "10"
+    assert_equal @cards[1].question, "What is Rachel's favorite animal?"
+    assert_equal @cards[1].answer, "red panda"
+    assert_equal @cards[2].question, "What is Mike's middle name?"
+    assert_equal @cards[2].answer, "nobody knows"
+    assert_equal @cards[3].question, "What cardboard cutout lives at Turing?"
+    assert_equal @cards[3].answer, "Justin Bieber"
   end
 
 end
